7b00ee3dc93f090cfffdd091c14ab0bec825e214
gocha.darjania <darjaniia@gmail.com>
2023-03-01 17:42:14 +0400

    add Middleware

--- Loan_API.Application/Common/Validation/ValidationBehaviour.cs
+++ Loan_API.Application/Common/Validation/ValidationBehaviour.cs
@@ -14,65 +14,21 @@ namespace Loan_API.Application.Common.Validation
   public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
   {
     private readonly IEnumerable<IValidator<TRequest>> _validators;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators, IHttpContextAccessor httpContextAccessor)
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
     {
       _validators = validators;
-      _httpContextAccessor = httpContextAccessor;
     }
     public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
     {
-      try
+      if (_validators.Any())
       {
-        var validationResults = _validators.Select(v => v.Validate(request));
-
-        var failures = validationResults
-            .SelectMany(r => r.Errors)
-            .Where(f => f != null)
-            .ToList();
-
-        if (failures.Any())
-        {
-          var errorList = failures.Select(f => new Error(f.PropertyName, f.ErrorMessage)).ToList();
-          var errorResponse = new ErrorResponse("One or more validation errors occurred.", errorList);
-
-          _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-          await _httpContextAccessor.HttpContext.Response.WriteAsJsonAsync(errorResponse);
-
-          return default;
-        }
-
-        return await next();
+        var context = new ValidationContext<TRequest>(request);
+        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+        if (failures.Count != 0)
+          throw new FluentValidation.ValidationException(failures);
       }
-      catch (Exception ex)
-      {
-        _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        await _httpContextAccessor.HttpContext.Response.WriteAsJsonAsync(new ErrorResponse(ex.Message));
-        return default;
-      }
-    }
-    public class Error
-    {
-      public string PropertyName { get; set; }
-      public string ErrorMessage { get; set; }
-
-      public Error(string propertyName, string errorMessage)
-      {
-        PropertyName = propertyName;
-        ErrorMessage = errorMessage;
-      }
-    }
-
-    public class ErrorResponse
-    {
-      public ErrorResponse(string message, List<Error> errors = null)
-      {
-        Message = message;
-        Errors = errors ?? new List<Error>();
-      }
-
-      public string Message { get; set; }
-      public List<Error> Errors { get; set; }
+      return await next();
     }
   }
 }
--- Loan_API/Middleware/LoggerMiddleware.cs
+++ Loan_API/Middleware/LoggerMiddleware.cs
@@ -2,30 +2,39 @@
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using System;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace Loan_API.Middleware
 {
   public class LoggerMiddleware
   {
-
     private readonly RequestDelegate _next;
-    private readonly ILogger _logger;
-    public LoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+    private readonly ILogger<LoggerMiddleware> _logger;
+
+    public LoggerMiddleware(RequestDelegate next, ILogger<LoggerMiddleware> logger)
     {
       _next = next;
-      _logger = loggerFactory.CreateLogger<LoggerMiddleware>();
+      _logger = logger;
     }
+
     public async Task InvokeAsync(HttpContext context)
     {
-
       try
       {
         await _next(context);
       }
       catch (Exception ex)
       {
-        _logger.LogError($"Something went wrong: {ex.Message}");
+        _logger.LogError(ex, "An unhandled exception occurred.");
 
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+        {
+          StatusCode = context.Response.StatusCode,
+          Message = string.Join(Environment.NewLine, ex.Message.Split("\r\n"))
+        }));
       }
     }
   }
